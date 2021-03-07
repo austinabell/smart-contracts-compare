3ba163cb85e324d096d6426e6e63b58422451e54
austinabell <austinabell8@gmail.com>
2021-03-07 11:27:39 -0500

    Implement contract

--- near/contracts/assembly/main.ts
+++ near/contracts/assembly/main.ts
@@ -1,20 +1,28 @@
-import { storage, logging, env, context } from "near-sdk-as";
+import { storage, logging, env, context, ContractPromiseBatch } from "near-sdk-as";
 import { values, ContentRecord } from "./model";
 
 // --- contract code goes below
 
 const OWNER_KEY = "owner";
 
+// TODO should not require calling a function to initialize state, look into lifecycle (no examples)
 export function init(): void {
+  assert(!storage.hasKey(OWNER_KEY));
   storage.set<string>(OWNER_KEY, context.predecessor);
 }
 
-export function incrementCounter(value: i32): void {
-  
-}
-
 export function purchase(route: string, content: string): void {
+  const deposit = context.attachedDeposit;
+  assert(!deposit.isZero(), "Deposit required to purchase route");
+  const existing = values.get(route);
+  if (existing) {
+    assert(deposit > existing.price, "Not enough deposit to purchase route")
 
+    ContractPromiseBatch.create(existing.owner).transfer(existing.price);
+  }
+
+  let rec: ContentRecord = { price: deposit, content, owner: context.predecessor };
+  values.set(route, rec)
 }
 
 export function getRoute(route: string): string | null {
@@ -23,5 +31,9 @@ export function getRoute(route: string): string | null {
 }
 
 export function withdraw(): void {
-  
+  let owner = storage.get<string>(OWNER_KEY);
+  if (owner) {
+    assert(context.predecessor, owner);
+    ContractPromiseBatch.create(owner).transfer(context.accountBalance);
+  }
 }
