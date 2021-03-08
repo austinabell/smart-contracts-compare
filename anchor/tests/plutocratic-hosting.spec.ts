58cd6670b42123a64a72787f10ced12fd545282e
austinabell <austinabell8@gmail.com>
2021-03-08 16:40:46 -0500

    Setup anchor template

---
+++ anchor/tests/plutocratic-hosting.spec.ts
@@ -0,0 +1,14 @@
+import * as anchor from '@project-serum/anchor';
+
+describe('plutocratic-hosting', () => {
+
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+
+  it('Is initialized!', async () => {
+    // Add your test here.
+    const program = anchor.workspace.PlutocraticHosting;
+    const tx = await program.rpc.initialize();
+    console.log("Your transaction signature", tx);
+  });
+});
